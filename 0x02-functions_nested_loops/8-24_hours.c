b3d22377da94421624dda98f65d8bb04d1b17d72
denniskoech1 <denniskipkoech19@gmail.com>
2022-10-20 18:37:01 +0000

    Task 8 fix not yet

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 			if (i < 10)
 			{
 				_putchar('0');
-				_putchar(i + '0')
+				_putchar(i + '0');
 			}
 			else if (i >= 10)
 			{
@@ -21,17 +21,21 @@ void jack_bauer(void)
 				_putchar((i % 10) + '0');
 			}
 			if (j < 10)
-			}
+			{
 				_putchar(':');
 				_putchar('0');
 				_putchar(j + '0');
 			}
-			else if (j > 10)
+			else(j > 10)
 			{
 				_putchar(':');
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
+
 		}
+
+	}
+
 }
 
