e4c10b27e5f28a58fa01760ced7e215f572400b4
denniskoech1 <denniskipkoech19@gmail.com>
2022-10-22 00:06:37 +0000

    Hot task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
-	if (a > b)
-	{
+if (a > b && a > c)
+{
+	largest = a;
+}
+else if (b > c && b > a)
+{
+	largest = b;
+}
+else if (c > b)
+{
+	largest = c;
+}
+else
+{
+	largest = b;
+}
 
-		if (b > c)
-			largest = a;
-		else if (a > c)
-			largest = a;
-		else
-			largest = c;
-	}
-	else
-	{
-		if (b < c)
-		       largest = c;
-		else
-			largest = b;
-	}
-	return (largest);
+return (largest);
 }
